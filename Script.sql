a7f87adef28227a4af8d5a69f25529e259a7ec0e
Aya Elsharawi <34321395+AyaElsharawy@users.noreply.github.com>
2021-03-19 01:48:06 +0200

    SQL script for the project

---
+++ Script.sql
@@ -0,0 +1,5 @@
+create database souq;
+create table product(id serial primary key,Name text, description text, image text, price numeric(15,6) not null);
+create table users(id serial primary key,first_name text,last_name  text, email text, password text not null,address text,role text, phone text);
+create table purchase_item(id serial  primary key, product_id int references product(id), count int);
+create table orders(id int primary key, user_id int references users(id),purchase_item_id int references purchase_item(id),address text);
