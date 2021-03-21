11ff5bed3c7ecbedcea617fe85b333225df7b253
Aya Elsharawi <34321395+AyaElsharawy@users.noreply.github.com>
2021-03-21 03:05:16 +0200

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -1,5 +1,6 @@
 create database souq;
-create table product(id serial primary key,Name text, description text, image text, price numeric(15,6) not null, quantity int);
+create table category (id serial primary key,name text);
+create table product(id serial primary key,Name text, description text, image text, price numeric(15,6) not null, quantity int,category_id int refernces category(id));
 create table users(id serial primary key,first_name text,last_name  text, username text, email text, password text not null,address text,role text, phone text, birthday date, balance numeric(15,6), job text);
 create table purchase_item(id serial  primary key, product_id int references product(id), count int);
 create table orders(id int primary key, user_id int references users(id),purchase_item_id int references purchase_item(id),address text,date date,price numeric(15,6), status text);
