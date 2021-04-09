e9f2f7309d27da2ebce1e2a3d630a354285dd565
antondev807 <75436662+antondev807@users.noreply.github.com>
2021-04-09 09:09:02 +0200

    Add files via upload

--- src/AddToCart.java
+++ src/AddToCart.java
@@ -12,23 +12,44 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author anton
  */
 public class AddToCart extends HttpServlet {
-    ArrayList<Integer> ids = new ArrayList<Integer>();
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        int Quantity = Integer.parseInt(request.getParameter("Quantity"));
+        HttpSession session=request.getSession(true);
+        ArrayList<Integer> ids = new ArrayList<Integer>();
+    ArrayList<Integer> count = new ArrayList<Integer>();
+    
+    String id=(String)request.getParameter("ID");
+        System.out.println("id"+ id);
+    String Quantity=(String)request.getParameter("Quantity");
+    System.out.println("Quantity"+Quantity);
+        ids.add(Integer.parseInt(id.trim()));
+        System.out.println("ids s "+ids.size());
+        count.add(Integer.parseInt(Quantity.trim() ));
+        System.out.println("count"+count.get(0));
+        //HttpSession test = request.getSession(false);
+        //System.out.print("hello"+test.getAttribute("userId"));
+        //int Quantity = Integer.parseInt(request.getParameter("Quantity"));
         //for (int i=0;i<Quantity;i++){
-            ids.add(Integer.parseInt(request.getParameter("ID")));
+            //String id=request.getParameter("ID");
+            //int x=Integer.parseInt(id.trim());
+            //ids=(ArrayList<Integer>)test.getAttribute("productID");
+            //ids.add(x);
+            //ids.add(6);
+            session.setAttribute("productID", ids);
+            session.setAttribute("count", count);
+            response.sendRedirect("Cart.jsp");
             
         //}
-        response.sendRedirect("home.jsp");
+        
         
     }
 
